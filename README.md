abb2e269e5323d2fa784598e8c444b89a9bed29f
lawlict <linqj6@outlook.com>
2020-03-25 17:46:46 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# End-to-end diarization loss
+This is the implement of PIT loss, FastPIT loss and OPTM loss for end-to-end diarization training. Acknowledge 
+
+### Prerequisites
+```
+Python >= 3.7
+Pytorch >= 1.3.1
+numpy >= 1.15.1
+```
+
+## Simple example:
+The loss directory is regarded as the python package. Usage:
+```
+import torch
+from loss import PITLoss, FastPITLoss, OPTMLoss
+
+B, T, n_class = 1, 4, 4     # batch_size * nframe * n_speaker
+device = 'cpu'
+pred = torch.rand(B, T, n_class).to(device)
+label = torch.randint(0, 2, (B, T, n_class)).float().to(device)
+
+criterion1 = PITLoss()
+criterion2 = FastPITLoss()
+criterion3 = OPTMLoss()
+loss1, assigned_label1 = criterion1(pred, label)
+loss2, assigned_label2 = criterion2(pred, label)
+loss3, assigned_label3 = criterion3(pred, label)
+print(loss1, loss2, loss3)
+print(assigned_label1)
+print(assigned_label2)
+print(assigned_label3)
+```
+
+## citation
+```
+Q. Lin et al.: Optimal Mapping Loss: A Faster Loss for End-to-End Speaker Diarization, Odyssey 2020.
+```
---
+++ loss/loss.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from itertools import permutations
+from .hungarian import Hungarian
+
+
+# modified for padding cases
+class BasicLoss():
+    ''' The basic class for pit, fastpit and optm loss.''' 
+    def __init__(self):
+        self.criterion = nn.BCELoss()
+
+    def __call__(self, preds, labels):
+        '''
+        args:
+            preds   - B * T * n_class, float
+            labels  - B * T * n_class, int
+        '''
+        losses = []
+        assigned_labels = []
+        nframe = 0
+        for pred, label in zip(preds, labels):
+            loss, assigned_label = self.minimize_loss(pred, label)
+            losses.append(loss * len(pred))
+            nframe += len(pred)
+            assigned_labels.append(assigned_label)
+
+        loss = torch.stack(losses).sum() / nframe
+        return loss, assigned_labels
+
+
+
+class PITLoss(BasicLoss):
+    ''' Minimize the loss by permutation of all possible labels.'''
+    def minimize_loss(self, pred, label):
+        n_class = label.shape[-1]
+        loss = None
+        assigned_label = None
+        for p in permutations(range(n_class)):
+            bce_loss = self.criterion(pred, label[:, p])
+            if loss is None or loss > bce_loss:
+                loss = bce_loss
+                assigned_label = label[:, p]
+        return loss, assigned_label
+
+
+
+class FastPITLoss(BasicLoss):
+    def __init__(self):
+        self.criterion = nn.BCELoss(reduction='none')
+
+    ''' Minimize the loss by pre-construction of loss matrix and permutation.'''
+    def minimize_loss(self, pred, label):
+        n_class = label.shape[-1]
+        # The faster construction of loss matrix, but 
+        # increase space complexity (T * n_class --> T * n_class ** 2).
+        ext_pred = pred.unsqueeze(2).repeat(1, 1, n_class)
+        ext_label = label.unsqueeze(1).repeat(1, n_class, 1)
+        loss_mat = self.criterion(ext_pred, ext_label)
+        loss_mat = loss_mat.mean(dim=0)
+        
+        loss = None
+        assigned_label = None
+        for p in permutations(range(n_class)):
+            bce_loss = loss_mat[range(n_class), p].mean()
+            if loss is None or loss > bce_loss:
+                loss = bce_loss
+                assigned_label = label[:, p]
+        return loss, assigned_label
+
+
+
+class OPTMLoss(BasicLoss):
+    def __init__(self):
+        self.criterion = nn.BCELoss(reduction='none')
+
+    ''' Minimize the loss by pre-construction of loss matrix and Hungarian algorithm.'''
+    def minimize_loss(self, pred, label):
+        n_class = label.shape[-1]
+        ext_pred = pred.unsqueeze(2).repeat(1, 1, n_class)
+        ext_label = label.unsqueeze(1).repeat(1, n_class, 1)
+        loss_mat = self.criterion(ext_pred, ext_label)
+        loss_mat = loss_mat.mean(dim=0)
+
+        H = Hungarian()
+        H.calculate(loss_mat.detach().cpu().numpy())
+        mappings = H.get_results()
+        p = [col for row, col in sorted(mappings)]
+        loss = loss_mat[range(n_class), p].mean()
+        assigned_label = label[:, p]
+        return loss, assigned_label
+
+
+
+if __name__ == '__main__':
+    B, T, n_class = 1, 4, 4
+    device = 'cpu'
+    pred = torch.rand(B, T, n_class).to(device)
+    label = torch.randint(0, 2, (B, T, n_class)).float().to(device)
+
+    criterion1 = PITLoss()
+    criterion2 = FastPITLoss()
+    criterion3 = OPTMLoss()
+    loss1, assigned_label1 = criterion1(pred, label)
+    loss2, assigned_label2 = criterion2(pred, label)
+    loss3, assigned_label3 = criterion3(pred, label)
+    print(loss1, loss2, loss3)
+    print(assigned_label1)
+    print(assigned_label2)
+    print(assigned_label3)
